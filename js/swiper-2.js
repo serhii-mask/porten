b754ab82cf95ff2cdcf428689491e72683897954
Serhii <serhii.mask@gmail.com>
2023-02-19 22:09:41 +0200

    add mobile version

---
+++ js/swiper-2.js
@@ -0,0 +1,12 @@
+// new Swiper(".swiper-2", {
+//    navigation: {
+//       nextEl: ".swiper-2-button-next",
+//       prevEl: ".swiper-2-button-prev",
+//    },
+
+//    pagination: {
+//       el: ".swiper-2-pagination",
+//       clickeble: true,
+//       dynamicBullets: true,
+//    },
+// });
